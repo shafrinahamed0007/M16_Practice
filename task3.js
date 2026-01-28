41d59b8c3a3a6e6feaffc5cd537b198ff2dce67a
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-01-28 16:58:39 +0600

    Complete object task4

---
+++ task3.js
@@ -0,0 +1,15 @@
+/**
+ * Display the physics marks as output
+ */
+
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    Physics: {
+        subject: "HSC Physics",
+        author: "Shajahan Tapan",
+        marks: 30
+    }
+}
+
+console.log(student.Physics.marks);
