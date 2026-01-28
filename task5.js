f1ea92ca3d464ad53d5b4d30cb8e1599d22c5ad2
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-01-28 17:30:58 +0600

    Complete Object Task5

---
+++ task5.js
@@ -0,0 +1,24 @@
+/**
+ * Loop trough an object and print key-value paris with their types
+ * 
+ * Output:
+
+
+key: name | type:  string
+key: age | type:  number
+key: city | type:  string
+key: isStudent | type:  boolean
+ * 
+ * 
+ */
+
+let myObjects = {
+  name: "John Doe",
+  age: 25,
+  city: "Example City",
+  isStudent: true,
+};
+
+for(key in myObjects){
+    console.log("key: ", key, "| type: ", typeof myObjects[key] );
+}
